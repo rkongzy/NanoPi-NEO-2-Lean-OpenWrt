78ce4d749832913958f3668ae48349ef0200f439
rkongzy <sherlock_zy@hrbeu.edu.cn>
2021-08-23 11:58:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.66/g' package/base-files/files/bin/config_generate
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
